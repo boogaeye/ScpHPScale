73475d70f24bdc759e88b2bc28c81e42052a2c86
boogaeye <bradleigh.christensen@makt3k.com>
2020-10-11 14:32:03 -0700

    Add project files.

---
+++ ScpHPScale-EXILED2/ScpHPScale.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.Events.Handlers;
+using Exiled.API.Features;
+using Exiled.API.Enums;
+
+namespace ScpHPScale_EXILED2
+{
+    public class ScpHPScale : Plugin<Config>
+    {
+        private static readonly Lazy
+    }
+}
